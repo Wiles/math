024eb385c4d91e0a6c7d7665d3a4041d4e47477b
Samuel Lewis <samuelewis@gmail.com>
2012-09-26 11:19:37 -0400

    Improved printing

--- assign_2/math.py
+++ assign_2/math.py
@@ -1,15 +1,16 @@
 import sys
+from texttable import *
 
 def file_to_matrix(file):
-	list = []
-	for line in open(file).readlines():
-		list.append([(float(x)) for x in line.split()])
-		
-	return list
+	return [([(float(x)) for x in line.split()]) for line in open(file).readlines()]
 
-def print_results(matrix):
-	for x in range(len(matrix)):
-		print (matrix[x][len(matrix[x]) - 1])
+def print_matrix(matrix):
+	table = Texttable()
+	table.set_precision(1)
+	table.set_deco(Texttable.HEADER)
+	table.set_cols_align([("r") for i in range(0, len(matrix[0]))])
+	[(table.add_row(row)) for row in matrix]
+	print table.draw()
 
 def do_math(matrix):
 	for x in range(len(matrix)):
@@ -22,4 +23,12 @@ def do_math(matrix):
 	return matrix
 	
 if __name__ == "__main__":
-	print_results(do_math(file_to_matrix(sys.argv[1])))
+	input = file_to_matrix(sys.argv[1])
+	print "Input Matrix:"
+	print_matrix(input)
+	print
+	
+	output = do_math(input)
+	
+	print "Output Matrix:"
+	print_matrix(output)
