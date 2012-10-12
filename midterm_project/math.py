70f42a0a6b63c5ca17a995075e0c155d41fdf8ba
Samuel Lewis <samuelewis@gmail.com>
2012-10-12 18:56:29 -0400

    Removed old animation code

--- midterm_project/math.py
+++ midterm_project/math.py
@@ -1,10 +1,4 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-
-def update_line(num, data, line):
-    line.set_data(data[...,:num])
-    return line,
     
 def do_points(interval, x_first_coef, x_second_coef,  y_first_coef, y_second_coef ):
     time = 0.0
@@ -21,18 +15,18 @@ def do_points(interval, x_first_coef, x_second_coef,  y_first_coef, y_second_coe
     return x, y
     	
 def do_math():
-
-    plt.xlabel('Distance')
-    plt.xlabel('Height')
-    plt.title('Golf Ball Flight')
     
     distance_air, height_air = do_points(0.01, 32, -0.1, 42, -4.9)
     plt.plot(distance_air, height_air, label="w/ air")
     distance, height = do_points(0.01, 32, -0.0, 42, -4.9)
     plt.plot(distance, height, label="w/o air")
-    plt.xlim(min(distance + distance_air), max(distance + distance_air) + 10)
-    plt.ylim(min(height + height_air), max(height + height_air) + 10)
+    plt.xlim(min(distance + distance_air) - 10, max(distance + distance_air) + 10)
+    plt.ylim(min(height + height_air) - 10, max(height + height_air) + 10)
     
+    plt.xlabel('Distance')
+    plt.xlabel('Height')
+    plt.title('Golf Ball Flight')
+    plt.grid(True)
     plt.legend()
     plt.show()
 
