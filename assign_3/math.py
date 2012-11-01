f18760c06c0083dee661791f9db2f6af0a26c954
Samuel Lewis <samuelewis@gmail.com>
2012-11-01 19:33:57 -0400

    Working assign 3

--- assign_3/math.py
+++ assign_3/math.py
@@ -3,14 +3,17 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import math
 
-def get_y(x, m, b):
-	return m * x + b
+def get_y(x, m, rc, b):
+	return math.exp(b) * (math.e ** ( - x/rc))
 	
-def get_ys(x, m, b):
-	return [get_y(_x, m, b) for _x in x]
+def get_ys(x, m, rc, b):
+	return [get_y(_x, m,rc, b) for _x in x]
 	
 def sq_sum(x_in):
 	return sum([x_out ** 2 for x_out in x_in])
+	
+def ln(l):
+	return [math.log(x) for x in l]
 	
 def diff(y1, y2):
 	return [y1[i] - y2[i] for i in range(len(y1))]
@@ -19,24 +22,36 @@ x = [2.0,4.0,6.0,8.0,10.0,12.0, 14.0,16.0,18.0,20.0,22.0,24.0,26.0,28.0,30.0]
 y = [9.7,8.1,6.6,5.1,4.4,3.7,2.8,2.4,2.0,1.6,1.4,1.1,0.85,0.69,0.6]
 
 n = len(x)
-xy = [x[i] * y[i] for i in range(n)]
-x_square = [x[i] ** 2 for i in range(n)]
+X = x
+Y = ln(y)
 
-m = (sum(xy)-((sum(x)*sum(y))/n))/(sum(x_square)-(sum(x)**2/(n)))
-b = (sum(y)/n)-m * (sum(x)/n)
+xy = [x[i] * y[i] for i in range(n)]
+XY = [X[i] * Y[i] for i in range(n)]
+m = (n * sum(XY) - (sum(X) * sum(Y)))/((n * sq_sum(X))-(sum(X) ** 2))
+rc = -1 /m
+b = ((sq_sum(X)) * (sum(Y)) - (sum(XY) * sum(X)))/((n * sq_sum(X))-(sum(X) ** 2))
+
+error = sq_sum(diff(y, get_ys(X, m, rc, b)))
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
 
 plt.xlim(min(x) - 1, max(x) + 1)
 plt.ylim(min(y) - 1, max(y) + 1)
 
-r = ((n * (sum(xy))) - (sum(x) * sum(y))) / (math.sqrt((n * sq_sum(x)) - (sum(x) ** 2)) * math.sqrt((n * sq_sum(y)) - (sum(y) ** 2)))
-print 'r = ' + str(r)
-error = sq_sum(diff(y, get_ys(x, m, b)))
-print 'e = ' + str(error)
-print 'y = ' + str(m) + 'x+' + str(b)
+a, = ax.plot(x, y, linestyle='none', marker='.')
 
-a, = plt.plot(x, y, linestyle='none', marker='.')
+xes = np.arange(min(x), max(x), 0.01)
 
-xes = np.arange(min(x) - 1, max(x) + 1, 0.01)
+a, = ax.plot(xes, get_ys(xes, m, rc, b))
 
-a, = plt.plot(xes, get_ys(xes, m, b))
+plt.xlabel('Time (s)')
+plt.ylabel('Voltage (v)')
+plt.title('Non-linear Curve Fitting')
+
+plt.annotate('E = ' + "%0.5f" % math.exp(b), xy=(min(x), min(y) + 2))
+plt.annotate('rc = ' + "%0.5f" % rc, xy=(min(x), min(y) + 1))
+plt.annotate('error = ' + "%0.5f" % error, xy=(min(x), min(y)))
+
 plt.show()
