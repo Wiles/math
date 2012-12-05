1aec2e563936a2753b53dc5c220d50b0de2c7d0a
Samuel Lewis <samuelewis@gmail.com>
2012-12-04 19:53:46 -0500

    Possible solution for assignment 5

--- assign_5/math.py
+++ assign_5/math.py
@@ -3,6 +3,7 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from matplotlib.colors import colorConverter
 import matplotlib.pyplot as plt
 from texttable import *
+import math
 
 cc = lambda arg: colorConverter.to_rgba(arg, alpha=1.0)
 
@@ -31,12 +32,23 @@ def draw_rectangular_prism(v, col):
     ax.add_collection3d(Poly3DCollection([[v[0], v[4], v[5], v[1]]], facecolors = [cc(col)]))
     ax.add_collection3d(Poly3DCollection([[v[3], v[7], v[6], v[2]]], facecolors = [cc(col)]))
     ax.add_collection3d(Poly3DCollection([[v[1], v[5], v[6], v[2]]], facecolors = [cc(col)]))
-    
-def rotate_rectangular_prism(v, deg, vert):
-    return v
 
-def translate_rectangular_prism(v, x, y, z):
-    return [[e[0] + x, e[1] + y, e[2] + z] for e in v]
+def do_math(deg, v, x, y, z):
+    vv = []
+    matrix = [
+        [math.cos(math.radians(60.0)), -math.sin(math.radians(60.0)), 0, x],
+        [math.sin(math.radians(60.0)), math.cos(math.radians(60.0)), 0, y],
+        [0,0,1,z],
+        [0,0,0,1]
+    ]
+    
+    for vector in v:
+        vv.append([
+            vector[0] * matrix[0][0] + vector[1] * matrix[0][1] + vector[2] * matrix[0][2] + 1 * matrix[0][3],
+            vector[0] * matrix[1][0] + vector[1] * matrix[1][1] + vector[2] * matrix[1][2] + 1 * matrix[1][3],
+            vector[0] * matrix[2][0] + vector[1] * matrix[2][1] + vector[2] * matrix[2][2] + 1 * matrix[2][3]
+        ])
+    return vv
     
 def print_points(v):
 	table = Texttable()
@@ -49,7 +61,8 @@ def print_points(v):
     
 draw_rectangular_prism(v, 'r')
 print_points(v)
-vv = translate_rectangular_prism(rotate_rectangular_prism(v, 60, 'z'), 2, 4, 1)
+vv = do_math(60.0, v, 2, 4, 1)
+print vv
 draw_rectangular_prism(vv, 'b')
 print
 print_points(vv)
